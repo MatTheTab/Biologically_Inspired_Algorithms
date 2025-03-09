eb121cd609af32370830bfc9d9a9f0e7811ede06
Bartłomiej Pukacki <bartek.pukacki@gmail.com>
2025-03-09 20:24:09 +0100

    Update organization

--- main.cpp
+++ main.cpp
@@ -4,34 +4,8 @@
 
 using namespace std;
 
-/**
- * @brief Test function, iterates over a double loop and assigns a value to a variable squared n times.
- * 
- * @param n Number of iterations of the double nested loop.
- */
-void test_func1(int n){
-    int val = 0;
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            val = i+j;
-        }
-    }
-}
-
-/**
- * @brief Test function, iterates over a double loop and assigns a value to a variable n x m times.
- * 
- * @param n Number of iterations of the outside loop.
- * @param m Number of iterations of the inner loop.
- */
-void test_func2(int n, int m){
-    int val = 0;
-    for (int i=0; i<n; i++){
-        for (int j=0; j<m; j++){
-            val = i+j;
-        }
-    }
-}
+void test_func1(int n);
+void test_func2(int n, int m);
 
 // Test with: g++ -o bio_alg main.cpp random.cpp -std=c++17
 // ./bio_alg
@@ -61,4 +35,33 @@ int main() {
     cout << "Speed test 1" << speed_test_1 << " nanoseconds" << "\n";
     cout << "Speed test 2" << speed_test_2 << " nanoseconds" << "\n";
     return 0;
+}
+
+/**
+ * @brief Test function, iterates over a double loop and assigns a value to a variable squared n times.
+ * 
+ * @param n Number of iterations of the double nested loop.
+ */
+void test_func1(int n){
+    int val = 0;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            val = i+j;
+        }
+    }
+}
+
+/**
+ * @brief Test function, iterates over a double loop and assigns a value to a variable n x m times.
+ * 
+ * @param n Number of iterations of the outside loop.
+ * @param m Number of iterations of the inner loop.
+ */
+void test_func2(int n, int m){
+    int val = 0;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            val = i+j;
+        }
+    }
 }
