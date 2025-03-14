c97eae4a880594f675edf5e2264a9b343372dd74
Bartłomiej Pukacki <bartek.pukacki@gmail.com>
2025-03-14 17:30:32 +0100

    Test problem loading

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "random.h"
 #include "time_measure.h"
+#include "Problem.h"
 
 using namespace std;
 
@@ -11,29 +12,33 @@ void test_func2(int n, int m);
 // ./bio_alg
 
 int main() {
-    srand(time(0));
-    int n = 10;
-    int* arr = generateRandomPerturbation(n);
-    cout << "Generated array: ";
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
-    }
-    cout << "\n";
-    delete[] arr;
+    string filename = "data/qap/bur26a.dat";
+    Problem problem(filename);
+    problem.displayInstance();
+ 
+    // srand(time(0));
+    // int n = 10;
+    // int* arr = generateRandomPerturbation(n);
+    // cout << "Generated array: ";
+    // for (int i = 0; i < n; i++) {
+    //     std::cout << arr[i] << " ";
+    // }
+    // cout << "\n";
+    // delete[] arr;
     
-    pair<int, int> randomValues = getRandomPair(n);
-    int first = randomValues.first;
-    int second = randomValues.second;
-    cout << "Random values: " << first << " and " << second << "\n";
+    // pair<int, int> randomValues = getRandomPair(n);
+    // int first = randomValues.first;
+    // int second = randomValues.second;
+    // cout << "Random values: " << first << " and " << second << "\n";
 
-    cout << "Time Measuring \n";
-    double speed_test_1;
-    double speed_test_2;
-    speed_test_1 = measureFunctionRuntime(test_func1, 1000);
-    speed_test_2 = measureFunctionRuntime(test_func2, 3000, 5000);
+    // cout << "Time Measuring \n";
+    // double speed_test_1;
+    // double speed_test_2;
+    // speed_test_1 = measureFunctionRuntime(test_func1, 1000);
+    // speed_test_2 = measureFunctionRuntime(test_func2, 3000, 5000);
 
-    cout << "Speed test 1" << speed_test_1 << " nanoseconds" << "\n";
-    cout << "Speed test 2" << speed_test_2 << " nanoseconds" << "\n";
+    // cout << "Speed test 1" << speed_test_1 << " nanoseconds" << "\n";
+    // cout << "Speed test 2" << speed_test_2 << " nanoseconds" << "\n";
     return 0;
 }
 
